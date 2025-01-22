d5b5331959d71be316fe52489a8c748434de6599
braden <leachbr787@northwested.org>
2025-01-22 13:48:58 -0500

    Create main_if_else.py

---
+++ main_if_else.py
@@ -0,0 +1,19 @@
+
+#Braden Leach
+#January 22 2025
+#Mystery Dictinary 
+
+input_name = input('Please enter your students name: ')
+#---Dict---#
+names_dict = {
+    "Smith":1234, 
+    "Leach":1342, 
+    "Lennon":5624
+    }
+
+
+if input_name not in names_dict: 
+    print('Person is not in our system!')
+else:
+    print(f'{input_name}\'s Student ID is : {names_dict[input_name]}')
+    
