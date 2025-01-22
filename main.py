00e5147ddcebc6077b246b9a21055acbb0ff7163
braden <leachbr787@northwested.org>
2025-01-22 13:33:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 
+#Braden Leach
+#January 22 2025
+#Mystery Dictinary 
+
+input_name = input('Please enter a name: ')
+
+#---Dict---#
+names_dict = {"Smith":1234, "Leach":1342, "Lennon":5624}
+
+#---Print---#
+print(f'Smith : {names_dict["Smith"]}')
+print(f'Leach : {names_dict["Leach"]}')
+print(f'Lennon : {names_dict["Lennon"]}')
+
+#---If not in dict---#
+unknown_names = names_dict.get('John','Requested key not found!')
+print(f"David : {unknown_names}")
+print()
+
+
+
+if input_name not in names_dict:
+    print('Requested key not found!') 
